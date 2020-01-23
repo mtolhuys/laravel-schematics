ed677cbc37ac969e0044b85c78d2454f96e8c4fc
Maarten Tolhuijs <mtolhuys@hotmail.com>
2020-01-23 21:02:31 +0100

    Position relationless models separate

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -45,13 +45,6 @@
             }[style]
         },
 
-        styleSettings: {
-            alwaysRespectStubs: true,
-            midpoint: 0.6,
-            cornerRadius: 3,
-            stub: 10,
-        },
-
         loading: function (loading = false) {
             $('.loading').toggle(loading);
         },
@@ -64,39 +57,80 @@
             return $(".action");
         },
 
+        $withRelations: function () {
+            return $(".model:not(.no-relations)");
+        },
+
+        $withoutRelations: function() {
+            let $withoutRelations = [];
+
+            Schematics.$models().each(function(i, el) {
+                let $model = $(el),
+                    noRelations = true,
+                    table = $model.data('table').toLowerCase();
+
+                for (const relationTable in Schematics.relations) {
+                    if (relationTable === table) {
+                        noRelations = false;
+                    }
+
+                    Schematics.relations[relationTable].forEach(function(relation) {
+                        if (relation.table === table) {
+                            noRelations = false;
+                        }
+                    });
+                }
+
+                if (noRelations) {
+                    $model.addClass('no-relations');
+                    $withoutRelations.push($model);
+                }
+            });
+
+            return $withoutRelations;
+        },
+
         getModelPosition: function (model) {
             return JSON.parse(localStorage.getItem(`schematics-settings-${model}-position`));
         },
 
-        positionModels: function () {
-            this.$models().each(function (i, el) {
-                let $model = $(el),
-                    model = $model.data('model').toLowerCase(),
-                    position = Schematics.getModelPosition($model);
+        positionModel: function (element) {
+            let $model = $(element),
+                model = $model.data('model').toLowerCase(),
+                position = Schematics.getModelPosition($model);
 
-                if (position) {
-                    position.top = position.top >= 80 ? position.top : 80;
-                    position.left = position.left >= 10 ? position.left : 10;
+            if (position) {
+                position.top = position.top >= 80 ? position.top : 80;
+                position.left = position.left >= 10 ? position.left : 10;
 
-                    $model.css(position);
+                $model.css(position);
+            } else {
+                const posX = ($model.width() + Schematics.minDistanceX),
+                    posY = ($model.height() + Schematics.minDistanceY);
+
+                $model.css(Schematics.position);
+
+                localStorage.setItem(
+                    `schematics-settings-${model}-position`,
+                    JSON.stringify(Schematics.position)
+                );
+
+                if (Schematics.position.left + (posX * 1.5) >= $(window).width()) {
+                    Schematics.position.left = 10;
+                    Schematics.position.top += posY;
                 } else {
-                    const posX = ($model.width() + Schematics.minDistanceX),
-                        posY = ($model.height() + Schematics.minDistanceY);
-
-                    $model.css(Schematics.position);
-
-                    localStorage.setItem(
-                        `schematics-settings-${model}-position`,
-                        JSON.stringify(Schematics.position)
-                    );
-
-                    if (Schematics.position.left + (posX * 1.5) >= $(window).width()) {
-                        Schematics.position.left = 10;
-                        Schematics.position.top += posY;
-                    } else {
-                        Schematics.position.left += posX;
-                    }
+                    Schematics.position.left += posX;
                 }
+            }
+        },
+
+        positionModels: function () {
+            Schematics.$withRelations().each(function (i, element) {
+                Schematics.positionModel(element);
+            });
+
+            Schematics.$withoutRelations().forEach(function (element) {
+                Schematics.positionModel(element);
             });
         },
 
@@ -131,7 +165,6 @@
                         });
                     }
                 });
-
             });
 
             Schematics.relationsModals();
