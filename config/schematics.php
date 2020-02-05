4a10da154e8d44dd8283790018d0afba72859080
Robbin Schepers <r.schepers@socialblue.com>
2020-02-05 11:47:38 +0100

    Adds possibility to generate with ::class instead of strings

--- config/schematics.php
+++ config/schematics.php
@@ -3,4 +3,9 @@
 return [
     'project-name' => null,
     'middleware' => null,
+
+    /* Decides if relation generator uses \App\Model::class or 'App\Model' for generating relations
+     * Possible options: 'string', 'class'
+     */
+    'relation-generator-method' => 'string',
 ];
