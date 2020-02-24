0285879a10c2b238a943c64704b058d7f0cb8d39
Maarten Tolhuijs <mtolhuys@hotmail.com>
2020-02-24 20:04:24 +0100

    Add browser-sync support

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,14 +1,19 @@
 const mix = require('laravel-mix');
 const tailwindcss = require('tailwindcss');
-const path = process.env.NODE_ENV === 'development' ?
-    // Path to my laravel's public folder
-    '../../public/vendor/schematics/'
-    : 'dist/';
+const dev = process.env.NODE_ENV === 'development';
+// Path to my laravel app's public folder
+const path = dev ? '../../public/vendor/schematics/' : 'dist/';
 
-mix.js('resources/js/app.js', path)
+if (dev && process.argv.includes('--sync')) {
+    mix.browserSync({
+        proxy: 'localhost:8000/schematics'
+    });
+}
+
+mix.setPublicPath(path)
+    .js('resources/js/app.js', path)
     .sass('resources/scss/app.scss', path)
-    .setPublicPath(path)
     .options({
         processCssUrls: false,
-        postCss: [tailwindcss('./tailwind.config.js')]
+        postCss: [tailwindcss('./tailwind.config.js')],
     });
