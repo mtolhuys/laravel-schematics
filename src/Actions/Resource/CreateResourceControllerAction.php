1bd7854a795f790ddf85d1effe3565379138389f
rpouls <info@roypouls.nl>
2020-03-19 14:39:27 +0100

    Update CreateResourceControllerAction.php
    On Laravel PHP 7.4 - Laravel 7.2 I got an STDIN constant exception when creating a ResourceController. Since we are not running this in a console, we can't give input anyway. So I would suggest to add this to all Artisan::call() calls.

--- src/Actions/Resource/CreateResourceControllerAction.php
+++ src/Actions/Resource/CreateResourceControllerAction.php
@@ -19,6 +19,7 @@ class CreateResourceControllerAction
             'name' => $name,
             '--resource' => true,
             '--model' => config('schematics.model.namespace').$model,
+            '-q',
         ]);
 
         return Artisan::output();
